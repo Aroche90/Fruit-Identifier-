032b63f36bb2d742160e099001bdacb34a68f43e
Amy White <adub238@gmail.com>
2020-02-21 20:16:28 -0800

    addingbackpages

---
+++ Amy/PyFiles/histogram_display.py
@@ -0,0 +1,14 @@
+# import the necessary packages
+from matplotlib import pyplot as plt
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+args = vars(ap.parse_args())
+
+# load the image and show it
+image = cv2.imread(args["image"])
+cv2.imshow("image", image)
